d3461a5a32cb6d6ef104df941e00f198b8450f01
skycutecat <les.miserables309@gmail.com>
2016-09-27 15:32:31 +0800

    edit note

--- gitNote.js
+++ gitNote.js
@@ -1,6 +1,4 @@
 
-
-
 git add
 git commit
 git commit -m "message"
@@ -42,6 +40,7 @@ git push // 若有預設的遠端 branch 就可以直接commit
 
 git push "remote_name" "local_branchname"
 
+git fetch (更新branch 最新的code至 local rep)
 
 
 //遇到衝突如何處理
@@ -53,3 +52,9 @@ AB 修改同個檔案, A先 push. 此時 B再push就會有衝突
 1. pull 回來
 2. merge
 3. push
+
+
+需要再加以理解的部分：
+1. fetch/clone vs pull
+2. local  git branch:  master vs branch, 為什麼bracn 異動檔案切換至 master,切換內容仍會連動
+3. pull  =  fetch + merge, 此時遇到衝突要如何處理呢？
